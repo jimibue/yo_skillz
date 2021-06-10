c1dd9013010908db11f2cd3ab0f46c40ab1c95b1
James Yeates <jimibue@gmail.com>
2021-06-10 11:48:53 -0600

    axios calls added

--- client/src/pages/Skills.js
+++ client/src/pages/Skills.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import ErrorMessage from '../components/ErrorMessage'
+import Spinner from '../components/Spinner'
+import List from '../components/List'
+import useAxiosOnMount from '../customHooks/useAxiosOnMount'
+import Skill from './Skill'
 
 const Skills = ()=>{
+    // about the useState hook
+    const {data,loading,error} = useAxiosOnMount('/api/skills')
+    // const {data,loading,error} = useAxiosOnMount('https://rasdfeqres.in/api/users?delay=3')
+
+    if(loading) return <Spinner />
+    if(error) return <ErrorMessage error={error}/>
     return (
         <div>
-            <h1>skills </h1>
+            <List 
+               renderData={(skill) => <Skill {...skill}/>}
+               data={data} 
+               name='Skills'/>
         </div>
     )
 }
--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import ErrorMessage from '../components/ErrorMessage'
+import Spinner from '../components/Spinner'
+import List from '../components/List'
+import useAxiosOnMount from '../customHooks/useAxiosOnMount'
 
 const Users = ()=>{
+    // about the useState hook
+    const {data,loading,error} = useAxiosOnMount('/api/users')
+    // const {data,loading,error} = useAxiosOnMount('https://rasdfeqres.in/api/users?delay=3')
+
+    if(loading) return <Spinner />
+    if(error) return <ErrorMessage error={error}/>
     return (
         <div>
-            <h1>users</h1>
+            <List 
+               renderData={(user)=>{
+                   return(
+                   <div>
+                       <h1>{user.name}</h1>
+                   </div>
+                   )
+               }}
+               data={data} 
+               name='Users'/>
         </div>
     )
 }
